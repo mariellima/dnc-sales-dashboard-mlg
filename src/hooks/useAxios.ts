08765948180d9de2f49228186bcce92c818c194b
mariellima <marielgomes1@outlook.com>
2025-08-29 16:26:21 -0300

    Feat: Home Page Requests

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,40 +1,75 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useState } from "react";
-import axios, { type AxiosRequestConfig } from "axios";
+import { useState, useEffect } from 'react'
+import axios, { type AxiosRequestConfig } from 'axios'
+import Cookies from 'js-cookie'
 
 
 const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/`,
-});
+})
 
 export const usePost = <T, P>(endpoint: string) => {
-    const [data, setData] = useState<T | null>(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState<number | null>(null);
-    
-    const postData = async (postData: P, config?: AxiosRequestConfig) => {
-        setData(null);
-        setLoading(true);
-        setError(null);
-        try {
-        const response = await axiosInstance({
-            url: endpoint,
-            method: "POST",
-            data: postData,
-            headers: { 
-                'Content-Type': 'application/json',
-                ...(config?.headers)
-            },
-            ...config,
-        });
-        setData(response.data);
-        } catch (e: any) {
-        setError(e.response.status ?? 500);
-        } finally {
-        setLoading(false);
-        }
-    };
-    
-    return { data, loading, error, postData };
+  const [data, setData] = useState<T | null>(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<number | null>(null)
 
+  const postData = async (postData: P, config?: AxiosRequestConfig) => {
+    setData(null)
+    setLoading(true)
+    setError(null)
+    try {
+      const response = await axiosInstance({
+        url: endpoint,
+        method: 'POST',
+        data: postData,
+        headers: {
+          'Content-Type': 'application/json',
+          ...config?.headers,
+        },
+        ...config,
+      })
+      setData(response.data)
+    } catch (e: any) {
+      setError(e.response.status ?? 500)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return { data, loading, error, postData }
+}
+
+export const useGet = <T>(endpoint: string, config?: AxiosRequestConfig) => {
+  const [data, setData] = useState<T | null>(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<number | null>(null)
+
+  const getData = async () => {
+    setLoading(true)
+    setError(null)
+
+    try {
+      const response = await axiosInstance({
+        url: endpoint,
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${Cookies.get('Authorization')}`,
+          ...config?.headers,
+        },
+        ...config,
+      })
+      setData(response.data)
+    } catch (e: any) {
+      setError(e.response.status ?? 500)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+  return { data, loading, error, getData }
 }
--- src/types/customChart.d.ts
+++ src/types/customChart.d.ts
@@ -2,5 +2,4 @@ export interface CustomChartProps {
     labels: string[];
     data: number[];
     type: 'line' | 'bar' ;
-    
 }
