9ab5ddc5846ab2f829e28b512ed708bb2e3d4991
mariellima <marielgomes1@outlook.com>
2025-09-02 07:49:11 -0300

    Feat: Profile Request

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -4,7 +4,6 @@ import { useState, useEffect } from 'react'
 import axios, { type AxiosRequestConfig } from 'axios'
 import Cookies from 'js-cookie'
 
-
 const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/`,
 })
@@ -54,7 +53,7 @@ export const useGet = <T>(endpoint: string, config?: AxiosRequestConfig) => {
         url: endpoint,
         method: 'GET',
         headers: {
-            'Authorization': `Bearer ${Cookies.get('Authorization')}`,
+          Authorization: `Bearer ${Cookies.get('Authorization')}`,
           ...config?.headers,
         },
         ...config,
@@ -67,9 +66,68 @@ export const useGet = <T>(endpoint: string, config?: AxiosRequestConfig) => {
     }
   }
 
-    useEffect(() => {
-        getData()
-    }, [])
+  useEffect(() => {
+    getData()
+  }, [])
 
   return { data, loading, error, getData }
+}
+
+export const usePut = <T>(endpoint: string) => {
+  const [data, setData] = useState<T | null>(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<number | null>(null)
+
+  const putData = async (putData: T, config?: AxiosRequestConfig) => {
+    setData(null)
+    setLoading(true)
+    try {
+      const response = await axiosInstance({
+        url: endpoint,
+        method: 'PUT',
+        data: putData,
+        headers: {
+          Authorization: `Bearer ${Cookies.get('Authorization')}`,
+          'Content-Type': 'application/json',
+          ...config?.headers,
+        },
+        ...config,
+      })
+      setData(response.data)
+    } catch (e: any) {
+      setError(e.response.status ?? 500)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return { data, loading, error, putData }
+}
+
+export const useDelete = <T>(endpoint: string) => {
+  const [data, setData] = useState<T | null>(null)
+  const [loading, setLoading] = useState(false)
+
+  const deleteData = async (config?: AxiosRequestConfig) => {
+    setData(null)
+    setLoading(true)
+    try {
+      const response = await axiosInstance({
+        url: endpoint,
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${Cookies.get('Authorization')}`,
+          ...config?.headers,
+        },
+        ...config,
+      })
+      setData(response.data)
+    } catch (e: any) {
+     throw e.response?.status
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return { data, loading, deleteData }
 }
---
+++ src/types/profileData.d.ts
@@ -0,0 +1,9 @@
+export interface ProfileEditableData {
+    name: string
+    phone: string
+
+}
+
+export interface ProfileData extends ProfileEditableData {
+    email: string
+}
